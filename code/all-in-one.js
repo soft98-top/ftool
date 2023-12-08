3d0b0fdf1315d3cb9b523fa9f9ee1149655ee211
soft98 <soft1998@qq.com>
2023-12-08 15:34:02 +0800

    增加funcToString，增加注释，完善jsonToObjc

--- code/all-in-one.js
+++ code/all-in-one.js
@@ -7,6 +7,10 @@
  */
 
 const obj_enum = {
+    /**
+     * 枚举对象的方法
+     * @param {string} className - 类名
+     */
     enumMethods: function (className) {
         // 枚举对象的方法
         let hookOwnMethods = ObjC.classes[className].$ownMethods;
@@ -16,6 +20,9 @@ const obj_enum = {
         send("methods: " + JSON.stringify(hookAllMethods));
         send("allClasses: " + JSON.stringify(hookClasses));
     },
+    /**
+     * 枚举模块
+     */
     enumMoudles:function () {
         // 枚举模块
         var modules = Process.enumerateModules();
@@ -23,6 +30,10 @@ const obj_enum = {
             send(`== Name: ${modules[i].name}  <${modules[i].base}>`);
         }
     },
+    /**
+     * 枚举模块导出的函数
+     * @param {string} moduleName - 模块名
+     */
     enumExports:function (moduleName) {
         // 枚举模块导出的函数
         var exports = Module.enumerateExports(moduleName);
@@ -30,6 +41,10 @@ const obj_enum = {
             send(`== Name: ${exports[i].name}  <${exports[i].address}>`);
         }
     },
+    /**
+     * 枚举模块导入的函数
+     * @param {string} moduleName - 模块名
+     */
     enumImports:function (moduleName) {
         // 枚举模块导入的函数
         var imports = Module.enumerateImports(moduleName);
@@ -40,6 +55,11 @@ const obj_enum = {
 }
 
 const obj_method = {
+    /**
+     * 获取方法的信息(参数类型，返回值类型)
+     * @param {string} className - 类名
+     * @param {string} methodName - 方法名
+     */
     getMethodInfo:function (className, methodName) {
         // 获取方法的信息(参数类型，返回值类型)
         let targetClass = ObjC.classes[className];
@@ -79,6 +99,11 @@ const obj_method = {
             }
         });
     },
+    /**
+     * 调用方法
+     * @param {string} className - 类名
+     * @param {string} methodName - 方法名
+     */
     invokeMethod:function (className, methodName) {
         // 调用方法
         let targetClass = ObjC.classes[className];
@@ -86,6 +111,12 @@ const obj_method = {
         let result = targetMethod();
         send(result);
     },
+    /**
+     * 传递参数调用方法，可变参数
+     * @param {string} className - 类名
+     * @param {string} methodName - 方法名
+     * @param {...any} args - 参数列表
+     */
     invokeMethodWithArgs:function (className, methodName, ...args) {
         // 传递参数调用方法，可变参数
         // 调用方法
@@ -94,6 +125,12 @@ const obj_method = {
         let result = targetMethod(...args);
         send(result);
     },
+    /**
+     * 链式无参调用方法
+     * @param {string} className - 类名
+     * @param {string} mustMethod - 必须调用的方法
+     * @param {...string} methodNames - 链式调用的方法名列表
+     */
     invokeMethodChain:function (className, mustMethod, ...methodNames) {
         // 链式无参调用方法
         // 调用方法
@@ -105,6 +142,12 @@ const obj_method = {
         );
         send(eval(invokeCmd));
     },
+    /**
+     * 根据模块名和偏移获取函数地址
+     * @param {string} module - 模块名
+     * @param {number} offset - 偏移量
+     * @returns {NativePointer} - 函数地址
+     */
     getFuncAddr:function (module, offset) {
         // 根据模块名和偏移获取函数地址
         // 根据名字获取module地址
@@ -129,6 +172,11 @@ const obj_method = {
         }));
         return return_addr;
     },
+    /**
+     * 获取目标函数的绝对地址
+     * @param {string} moduleName - 模块名
+     * @param {number} offset - 偏移量
+     */
     getMethodInfoByOffset:function (moduleName, offset) {
         // 获取目标函数的绝对地址
         var func_addr = obj_method.getFuncAddr(moduleName, offset);
@@ -164,6 +212,12 @@ const obj_method = {
             }
         });
     },
+    /**
+     * 获取方法的信息(参数类型，返回值类型)
+     * @param {string} className - 类名
+     * @param {string} methodName - 方法名
+     * @param {object} cmds - 钩子命令
+     */
     hookMethod:function (className, methodName, cmds) {
         // 获取方法的信息(参数类型，返回值类型)
         let targetClass = ObjC.classes[className];
@@ -197,6 +251,12 @@ const obj_method = {
             }
         });
     },
+    /**
+     * 获取目标函数的绝对地址
+     * @param {string} moduleName - 模块名
+     * @param {number} offset - 偏移量
+     * @param {object} cmds - 钩子命令
+     */
     hookMethodByOffset:function (moduleName, offset, cmds) {
         // 获取目标函数的绝对地址
         let func_addr = getFuncAddr(moduleName, offset);
@@ -217,6 +277,10 @@ const obj_method = {
             }
         });
     },
+    /**
+     * 获取类的所有方法的信息
+     * @param {string} className - 类名
+     */
     getAllMethodsInfo:function (className){
         let ownMethods = ObjC.classes[className].$ownMethods;
         for(let method of ownMethods){
@@ -226,13 +290,36 @@ const obj_method = {
 }
 
 const obj_util = {
+    /**
+     * JSON转OC对象
+     * @param {string} data - JSON字符串
+     * @returns {object} - OC对象
+     */
     jsonToObjc:function (data) {
+        // 判断data是不是object，如果是转换为字符串
+        if (typeof data === 'object') {
+            data = JSON.stringify(data);
+        }
+        
         // JSON转OC对象
         let NSString = ObjC.classes.NSString;
         let NSJSONSerialization = ObjC.classes.NSJSONSerialization;
         let strData = NSString.stringWithString_(data).dataUsingEncoding_(0x4);
         return NSJSONSerialization.JSONObjectWithData_options_error_(strData, 0x1, ptr(0x0));
-    }
+    },
+    /**
+     * 函数代码提取
+     * @param {function} func - 函数对象
+     * @returns {string} - 函数代码体
+     */
+    funcToString:function (func) {
+        // 函数转字符串
+        let func_str =  func.toString();
+        let regex = /{([\s\S]*)}/;
+        let matches = func_str.match(regex);
+        let extractedCode = matches[1];
+        return extractedCode;
+    },
 }
 
 const obj = {
@@ -241,6 +328,10 @@ const obj = {
     util:obj_util
 }
 
+/**
+ * 打印对象或函数
+ * @param {object|function} obj - 对象或函数
+ */
 function printObj(obj){
     if (typeof obj === 'function')
         send(obj.toString())
@@ -249,6 +340,10 @@ function printObj(obj){
     }
 }
 
+/**
+ * 处理执行命令
+ * @param {string} data - 命令字符串
+ */
 function handleExec(data){
     try{
         send(eval(data));
