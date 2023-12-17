41c75a48e970e46b656a6df856b66e5b82c3ed66
soft98 <soft1998@qq.com>
2023-12-17 13:10:10 +0800

    add surge code

---
+++ code/surge.js
@@ -0,0 +1,9 @@
+let surge_version_payload = {
+    "5.3.0":0x19546a,
+    "5.4.3":0x1994ae,
+    "4.11.2":0x165463
+}
+let retval_to_1 = "retval.replace(ptr(0x1));";
+
+let version = "4.11.2";
+obj.method.hookMethodByOffset("Surge",surge_version_payload[version],{"onLeave":retval_to_1});
