de39736eed92d200d5945631d27985cce47d0eff
soft98 <soft1998@qq.com>
2024-01-01 08:55:57 +0800

    增加版本自动识别

--- code/surge.js
+++ code/surge.js
@@ -4,6 +4,7 @@ let surge_version_payload = {
     "4.11.2":0x165463
 }
 let retval_to_1 = "retval.replace(ptr(0x1));";
-
-let version = "4.11.2";
+let key = ObjC.classes.NSString.stringWithString_("CFBundleShortVersionString");
+let version = ObjC.classes.NSBundle.mainBundle().infoDictionary().valueForKey_(key).toString();
+send(version)
 obj.method.hookMethodByOffset("Surge",surge_version_payload[version],{"onLeave":retval_to_1});
