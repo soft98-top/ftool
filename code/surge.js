d8fb2ec7aa173ce4f528548c9128ca81f519176a
soft98 <soft1998@qq.com>
2024-01-02 10:05:12 +0800

    update

--- code/surge.js
+++ code/surge.js
@@ -4,7 +4,6 @@ let surge_version_payload = {
     "4.11.2":0x165463
 }
 let retval_to_1 = "retval.replace(ptr(0x1));";
-let key = ObjC.classes.NSString.stringWithString_("CFBundleShortVersionString");
-let version = ObjC.classes.NSBundle.mainBundle().infoDictionary().valueForKey_(key).toString();
+let version = obj.util.getVersion();
 send(version)
 obj.method.hookMethodByOffset("Surge",surge_version_payload[version],{"onLeave":retval_to_1});
