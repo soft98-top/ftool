f87216e6db6217b04e315c31924068b529d7d670
soft98-top <soft1998@qq.com>
2024-08-19 18:09:33 +0800

    add support of Apple Silicon

--- code/surge.js
+++ code/surge.js
@@ -1,9 +1,15 @@
 let surge_version_payload = {
-    "5.3.0":0x19546a,
-    "5.4.3":0x1994ae,
-    "4.11.2":0x165463
+    "x86_64":{
+        "5.3.0":0x19546a,
+        "5.4.3":0x1994ae,
+        "4.11.2":0x165463
+    },
+    "arm64":{
+        "4.11.2":0x127100
+    }
 }
 let retval_to_1 = "retval.replace(ptr(0x1));";
 let version = getVersion();
-send(version)
-hookMethodByOffset("Surge",surge_version_payload[version],{"onLeave":retval_to_1});
+let arch = getArch();
+send(arch + ":" + version)
+hookMethodByOffset("Surge",surge_version_payload[arch][version],{"onLeave":retval_to_1});
